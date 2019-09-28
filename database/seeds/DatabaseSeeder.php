4f0a0fd97666d87649c4c75c43f13ad98f3bed66
Christopher <christophervistal25@gmail.com>
2019-09-28 19:34:18 +0800

    Remove factory for doctor

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,7 +11,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Doctor')->create();
+        // factory('App\Doctor')->create();
         $this->call([
             AccountsSeeder::class,
             Doctor::class,
