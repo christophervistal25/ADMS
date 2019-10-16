95eafece7752d762de8f32ec9bcbdca24e1fe88d
Christopher <christophervistal25@gmail.com>
2019-10-16 12:54:55 +0800

    Change the name from ADIS to APIT

--- resources/views/templates/dashboard-template.blade.php
+++ resources/views/templates/dashboard-template.blade.php
@@ -5,7 +5,7 @@
         <div class="col-md-3 left_col">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a  class="site_title text-center"><i class="fas fa-hospital"></i> <span>ADIS</span></a>
+              <a  class="site_title text-center"><i class="fas fa-hospital"></i> <span>APIT</span></a>
             </div>
 
             <div class="clearfix"></div>
